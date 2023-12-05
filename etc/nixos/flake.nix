03aad6d7f9c16fd9e6098f55aee9b914b4ac9734
Riccardo Sirchia <sirchia.r@gmail.com>
2023-12-05 23:53:23 +0100

    Cleaned up formatting

--- etc/nixos/flake.nix
+++ etc/nixos/flake.nix
@@ -28,28 +28,28 @@
       inherit system; 
       specialArgs = { inherit inputs; };
       modules = [
-          # Overlays-module makes "pkgs.unstable" available in configuration.nix
-          ({ config, pkgs, ... }: { nixpkgs.overlays = [ overlay-unstable ]; })
+        # Overlays-module makes "pkgs.unstable" available in configuration.nix
+        ({ config, pkgs, ... }: { nixpkgs.overlays = [ overlay-unstable ]; })
 
-          # Patch Fish shell issue until Fish shell 3.6.2 or 3.7.0 is released
-	  ({ config, pkgs, ... }: { nixpkgs.overlays = [
-	      (final: prev: {
-		fish = prev.fish.overrideAttrs (o: {
-		patches = (o.patches or [ ]) ++ [
-		  (pkgs.fetchpatch {
-		    name = "fix-zfs-completion.path";
-		    url = "https://github.com/fish-shell/fish-shell/commit/85504ca694ae099f023ae0febb363238d9c64e8d.patch";
-		    sha256 = "sha256-lA0M7E/Z0NjuvppC7GZA5rWdL7c+5l+3SF5yUe7nEz8=";
-		  })
-		];
-	      });
-	      })
-	    ];
-          })
-          ./configuration.nix
-          disko.nixosModules.disko
-          impermanence.nixosModules.impermanence
-          inputs.flake-programs-sqlite.nixosModules.programs-sqlite
+        # Patch Fish shell issue until Fish shell 3.6.2 or 3.7.0 is released
+				({ config, pkgs, ... }: { nixpkgs.overlays = [
+					(final: prev: {
+						fish = prev.fish.overrideAttrs (o: {
+							patches = (o.patches or [ ]) ++ [
+								(pkgs.fetchpatch {
+									name = "fix-zfs-completion.path";
+									url = "https://github.com/fish-shell/fish-shell/commit/85504ca694ae099f023ae0febb363238d9c64e8d.patch";
+									sha256 = "sha256-lA0M7E/Z0NjuvppC7GZA5rWdL7c+5l+3SF5yUe7nEz8=";
+								})
+							];
+						});
+					})
+				]; })
+
+        ./configuration.nix
+        disko.nixosModules.disko
+        impermanence.nixosModules.impermanence
+        inputs.flake-programs-sqlite.nixosModules.programs-sqlite
       ];
     };
   };
