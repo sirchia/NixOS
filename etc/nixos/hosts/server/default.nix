6b9855438234d962a8d556c7c13b5493c6311a21
Riccardo Sirchia <sirchia.r@gmail.com>
2024-10-29 22:24:26 +0100

    Added udev rules to allow rootless pass through of mouse and oculus to qemu VMs

--- etc/nixos/hosts/server/default.nix
+++ etc/nixos/hosts/server/default.nix
@@ -6,6 +6,8 @@
   # set scheduler for disks with zfs partitions
   services.udev.extraRules = ''
     KERNEL=="sd[a-z]*[0-9]*|mmcblk[0-9]*p[0-9]*|nvme[0-9]*n[0-9]*p[0-9]*", ENV{ID_FS_TYPE}=="zfs_member", ATTR{../queue/scheduler}="none"
+    SUBSYSTEM=="usb", ATTRS{idVendor}=="1532", ATTRS{idProduct}=="0099", GROUP="libvirtd"
+    SUBSYSTEM=="usb", ATTRS{idVendor}=="2833", ATTRS{idProduct}=="0186", GROUP="libvirtd"
   '';
 
   boot.kernelParams = [
